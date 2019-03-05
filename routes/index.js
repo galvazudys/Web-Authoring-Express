4034bc514b3020a7f23aa5ea76816337019dc899
Galvazudis <galvazudis705@gmail.com>
2019-03-05 07:46:47 +0000

    added new pages and moved javascript

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WorkForcers' });
+});
+
+router.get('/about_product', function(req, res, next) {
+  res.render('about_product', { title: 'WorkForcers' });
+});
+
+router.get('/about_team', function(req, res, next) {
+  res.render('about_team', { title: 'WorkForcers' });
+});
+
+router.get('/testimonials', function(req, res, next) {
+  res.render('testimonials', { title: 'WorkForcers' });
+});
+
+router.get('/contacts', function(req, res, next) {
+  res.render('contacts', { title: 'WorkForcers' });
+});
+
+router.get('/pricing', function(req, res, next) {
+  res.render('index', { title: 'WorkForcers' });
+});
+
+router.get('/pricing', function(req, res, next) {
+  res.render('index', { title: 'WorkForcers' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'WorkForcers' });
 });
 
 module.exports = router;
